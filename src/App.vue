559b5a8c56e3d51bb54597748a926724f0e37184
Cem Yildiz <cem.yildiz@tracksfortrucks.com>
2021-04-22 23:19:07 +0200

    added reveal lib

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Cover />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cover from "@/components/slide_templates/Cover";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cover
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/slide_templates/Cover.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="reveal">
+    <Reveal>
+    </Reveal>
+  </div>
+</template>
+
+<script>
+import Reveal from 'reveal/index'
+
+export default {
+  name: "Cover",
+  components: {
+    Reveal
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
